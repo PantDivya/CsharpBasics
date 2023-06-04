7fb4532487a18a11705e42b7251ba11aed7f2000
PantDivya <111637767+PantDivya@users.noreply.github.com>
2023-06-04 21:49:28 +0545

    Add files via upload

---
+++ CSharpTest/CSharpTest/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace CSharpTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string FirstName = "Divya";
+            string LastName = "Pant";
+            //Create a variable of type string and assign it a value
+            string greetings = "Have a nice day!";
+            Console.WriteLine(greetings);
+            Console.WriteLine("The length is: " + greetings.Length);
+            Console.WriteLine("In upper case: " + greetings.ToUpper());
+            Console.WriteLine("In lower case: " + greetings.ToLower());
+
+            //Concatenation
+            string Name = FirstName + LastName;
+            Console.WriteLine(Name);
+
+            //Interpolation
+            string name = $"My name is { FirstName} { LastName }";
+            Console.WriteLine(name);
+
+            Console.ReadLine();
+
+        }
+    }
+}
