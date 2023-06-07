dff1ee535b04f67c0cca90844d923f8cc27c0f2a
PantDivya <111637767+PantDivya@users.noreply.github.com>
2023-06-07 21:01:07 +0545

    Update4 Program.cs

--- CSharpTest/CSharpTest/Program.cs
+++ CSharpTest/CSharpTest/Program.cs
@@ -63,7 +63,32 @@ namespace CSharpTest
             {
                 Console.WriteLine("Name not found");
             }
-
+//Switch statement
+            int day = 3;
+            switch (day)
+            {
+                case 1:
+                    Console.WriteLine("Monday");
+                    break;
+                case 2:
+                    Console.WriteLine("Tuesday");
+                    break;
+                case 3:
+                    Console.WriteLine("Wednesday");
+                    break;
+                case 4:
+                    Console.WriteLine("Thursday");
+                    break;
+                case 5:
+                    Console.WriteLine("Friday");
+                    break;
+                case 6:
+                    Console.WriteLine("Saturday");
+                    break;
+                case 7:
+                    Console.WriteLine("Sunday");
+                    break;
+            }
             Console.ReadLine();
 
         }
